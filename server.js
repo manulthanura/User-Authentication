7b49929a93d66a22ae7431645b5939de52d1797e
Manul Thanura <manulkarunarathna@gmail.com>
2023-04-08 08:19:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const express = require("express")
 const app = express()
 const PORT = 5000
 app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
+
+const connectDB = require("./db");
+
+//Connecting the Database
+connectDB();
+
+const server = app.listen(PORT, () =>
+  console.log(`Server Connected to port ${PORT}`)
+)
+// Handling Error
+process.on("unhandledRejection", err => {
+  console.log(`An error occurred: ${err.message}`)
+  server.close(() => process.exit(1))
+})
